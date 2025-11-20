92e515600636cbd07ab6ca833f2dbc039b54b6b6
chenlong <hoccl@sina.com>
2025-11-20 17:09:14 +0800

    feat: 添加LLM模型DTO

---
+++ src/dtos/llm/ModelDTO.ts
@@ -0,0 +1,18 @@
+/**
+ * 模型DTO
+ */
+export class ModelDTO {
+
+    label: string;
+
+    id: string;
+
+    constructor(
+        label: string,
+        id: string
+    ) {
+        this.label = label;
+        this.id = id;
+    }
+
+}
