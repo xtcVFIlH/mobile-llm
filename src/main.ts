d57f3f98044ffeb22363e4a00c7c799e8aa1674e
xtcVFIlH <hoccl@sina.com>
2025-11-22 12:22:02 +0800

    feat: 提供ApiConfigService以支持依赖注入

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,8 @@ const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
 
+import { ApiConfigServiceKey } from "./interfaces/service/ApiConfigServiceInterface";
+import { ApiConfigService } from "./components/service/local/ApiConfigService";
+app.provide(ApiConfigServiceKey, new ApiConfigService());
+
 app.mount('#app');
