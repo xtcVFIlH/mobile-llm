f09a106bfc9cc4b7b7f0ee58c43157af47f8a7dd
xtcVFIlH <hoccl@sina.com>
2025-11-22 12:15:01 +0800

    feat: add TypeScript configuration and update Vite config with component resolvers

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        Components({
+            resolvers: [
+                VantResolver(),
+            ],
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            'stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+            'pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+            'components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        },
+    },
 })
