98375f1707815b78524906c1e832968bc7034249
xtcVFIlH <hoccl@sina.com>
2025-11-22 12:19:58 +0800

    feat: 为ApiConfigDTO添加字段，并将所有字段设置为只读
    - id: string
    - name: string

--- src/dtos/llm/ApiConfigDTO.ts
+++ src/dtos/llm/ApiConfigDTO.ts
@@ -3,11 +3,22 @@
  */
 export class ApiConfigDTO {
 
-    apiKey: string;
+    /** 配置ID */
+    readonly id: string;
+
+    /** 配置名称 */
+    readonly name: string;
+
+    /** 服务商提供的API key */
+    readonly apiKey: string;
 
     constructor(
+        id: string,
+        name: string,
         apiKey: string
     ) {
+        this.id = id;
+        this.name = name;
         this.apiKey = apiKey;
     }
 
